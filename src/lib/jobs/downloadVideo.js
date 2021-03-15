c52ccb0ae95b1b6a965676236f040cf070719254
Gabriel Eloy <gabrieleloy@pop-os.localdomain>
2021-03-14 22:46:27 -0300

    wip

--- src/lib/jobs/downloadVideo.js
+++ src/lib/jobs/downloadVideo.js
@@ -13,27 +13,31 @@ module.exports = {
     async handle({ data }) {
         const { url, startTime, duration, email, fileName } = data;
 
+        console.log('DOWNLOADING')
         await redisSet(fileName, { url, s3Link: '', status: 'DOWNLOADING' })
         await alertStatus(fileName, { status: 'DOWNLOADING' });
-
+        
         const videoPath = await download(url, fileName);
-
+        
+        console.log('EXTRACTING_AUDIO')
         await redisSet(fileName, { status: 'EXTRACTING_AUDIO' });
         await alertStatus(fileName, { status: 'EXTRACTING_AUDIO' });
-
+        
         const audioPath = await extractAudio(videoPath);
-
+        
         if (startTime || duration) {
             const editedAudioPath = await trimAudio({ filePath: audioPath, startTime, duration });
             await deleteFile(audioPath)
             await rename(editedAudioPath)
         }
-
+        
+        console.log('UPLOADING_TO_S3')
         await redisSet(fileName, { status: 'UPLOADING_TO_S3' });
         await alertStatus(fileName, { status: 'UPLOADING_TO_S3' });
-
+        
         const s3Link = await uploadToS3(audioPath);
-
+        
+        console.log('DONE')
         await redisSet(fileName, { status: 'DONE', s3Link: s3Link.Location })
         await alertStatus(fileName, { status: 'DONE', s3Link: s3Link.Location });
     }
